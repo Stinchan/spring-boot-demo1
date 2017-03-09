e8ea9ea6c99fccc3b3df1d9a02a6936d3e73065f
Stinchan <stc9210@qq.com>
2017-03-09 17:56:24 +0800

    first spring-boot demo.

---
+++ src/main/java/com/rat/controller/MyRat.java
@@ -0,0 +1,26 @@
+package com.rat.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Stinchan on 2017/3/9.
+ */
+@RestController
+@EnableAutoConfiguration
+public class MyRat {
+    @RequestMapping(value = "index", method = RequestMethod.GET)
+    public String index() {
+        return "this is my rat.";
+    }
+
+    @RequestMapping(value = "index/{name}", method = RequestMethod.GET)
+    public String indexWithName(@PathVariable String name) {
+        return "spring-boot with name:" + name;
+    }
+
+
+}
